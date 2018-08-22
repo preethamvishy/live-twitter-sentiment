adc16298067d8cfa0e8e1e0f81de5447d5996b80
Preetham Viswanathan <preethamvishy10@gmail.com>
2018-08-22 19:41:17 -0400

    Config update

--- app/config.js
+++ app/config.js
@@ -1,7 +1,7 @@
 export default {
-      consumer_key: process.env.consumer_key,
-      consumer_secret: process.env.consumer_key,
-      access_token: process.env.access_token,
-      access_token_secret: process.env.access_token_secret,
-      timeout_ms: 60 * 1000
+    consumer_key = process.env.consumer_key,
+    consumer_secret = process.env.consumer_secret,
+    access_token = process.env.access_token,
+    access_token_secret = process.env.access_token_secret,
+    timeout_ms = 60 * 1000
 }
